77770c5a6b0f43a4173592ab80cf2df56b9487b6
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-02-21 20:36:30 +0100

    example/test.sh: Improved tests of the debug executable

--- example/test.sh
+++ example/test.sh
@@ -19,21 +19,29 @@ HFUZZ_RUN_ARGS="-v -N 10000000 --run_time 120 --exit_upon_crash" cargo hfuzz run
 # verify that the fuzzing process found the crash
 test "$(cat $workspace/*.fuzz)" = "qwerty"
 
-# clean
-cargo hfuzz clean
-
-# verify that the hfuzz_target has been cleaned
-test ! -e hfuzz_target
-
 # build example in debug mode
 cargo hfuzz build-debug --verbose
 
-# test that the debug executable exists
-test -x hfuzz_target/x86_64-unknown-linux-gnu/debug/example
+# try to launch the debug executable without the crash file, it should fail with error code 1
+set +e
+hfuzz_target/*/debug/example
+status=$?
+set -e
+test $status -eq 1
+
+# try to launch the debug executable with the crash file, it should fail with error code 101
+set +e
+CARGO_HONGGFUZZ_CRASH_FILENAME=$(echo $workspace/*.fuzz) hfuzz_target/*/debug/example
+status=$?
+set -e
+test $status -eq 101
 
 # clean
 cargo hfuzz clean
 rm -rf hfuzz_workspace
+
+# verify that the hfuzz_target has been cleaned
+test ! -e hfuzz_target
 
 # verify that the hfuzz_target has been cleaned
 test ! -e hfuzz_target
