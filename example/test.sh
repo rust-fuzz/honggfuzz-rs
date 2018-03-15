9f1256fa555d51adf8781e59c56ab85b5de643f2
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-03-15 15:29:49 +0100

    example/test.sh: fix running without instrumentation

--- example/test.sh
+++ example/test.sh
@@ -54,7 +54,7 @@ RUSTFLAGS="" cargo build
 
 # but when we run it, it should fail with a useful error message and status 17
 set +e
-cargo run
+RUSTFLAGS="" cargo run
 status=$?
 set -e
 test $status -eq 17
