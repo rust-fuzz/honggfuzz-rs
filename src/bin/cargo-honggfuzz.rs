1cde097d41d10801f0f3b8e4df04adcce3d3dbd6
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-03-25 11:25:27 +0200

    Convert tabs to spaces

--- src/bin/cargo-honggfuzz.rs
+++ src/bin/cargo-honggfuzz.rs
@@ -8,21 +8,21 @@ const HONGGFUZZ_TARGET: &'static str = "hfuzz_target";
 compile_error!("honggfuzz-rs does not currenlty support Windows but works well under WSL (Windows Subsystem for Linux)");
 
 fn main() {
-	let mut args = env::args().skip(1);
-	if args.next() != Some("honggfuzz".to_string()) {
-		eprintln!("please launch as a cargo subcommand: \"cargo honggfuzz ...\"");
-		process::exit(1);
-	}
+    let mut args = env::args().skip(1);
+    if args.next() != Some("honggfuzz".to_string()) {
+        eprintln!("please launch as a cargo subcommand: \"cargo honggfuzz ...\"");
+        process::exit(1);
+    }
 
-	// add some flags to sanitizers to make them work with Rust code
-	let asan_options = env::var("ASAN_OPTIONS").unwrap_or_default();
-	let asan_options = format!("detect_odr_violation=0:{}", asan_options);
+    // add some flags to sanitizers to make them work with Rust code
+    let asan_options = env::var("ASAN_OPTIONS").unwrap_or_default();
+    let asan_options = format!("detect_odr_violation=0:{}", asan_options);
 
-	let tsan_options = env::var("TSAN_OPTIONS").unwrap_or_default();
-	let tsan_options = format!("report_signal_unsafe=0:{}", tsan_options);
+    let tsan_options = env::var("TSAN_OPTIONS").unwrap_or_default();
+    let tsan_options = format!("report_signal_unsafe=0:{}", tsan_options);
 
-	let command = format!("{}/honggfuzz", HONGGFUZZ_TARGET);
-	Command::new(&command) // exec honggfuzz replacing current process
+    let command = format!("{}/honggfuzz", HONGGFUZZ_TARGET);
+    Command::new(&command) // exec honggfuzz replacing current process
         .args(args)
         .env("ASAN_OPTIONS", asan_options)
         .env("TSAN_OPTIONS", tsan_options)
