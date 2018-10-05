7afda3e3540de304b7de12868e1bb90bddf72aa5
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-10-05 17:14:42 +0200

    Only use the linker hack when needed/available

--- example/test.sh
+++ example/test.sh
@@ -2,7 +2,13 @@
 export RUST_BACKTRACE=full
 
 # HACK: temporary fix, see https://github.com/rust-lang/rust/issues/53945#issuecomment-426824324
-export RUSTFLAGS="-Clink-arg=-fuse-ld=gold"
+set +e
+which ld.gold # test if we are on a system with the GNU Gold linker installed
+status=$?
+set -e
+if [ $status -eq 0 ] ;then # if so, use it
+  export RUSTFLAGS="-Clink-arg=-fuse-ld=gold"
+fi
 
 cargo clean
 cargo update
