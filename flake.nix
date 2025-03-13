bde524fdaae5d98fe1f026b77e78f4c2bed41f9f
Paul Grandperrin <paul.grandperrin@gmail.com>
2025-03-13 14:40:34 +0100

    flake: use flake-parts and devenv and work around glibc issue

--- flake.nix
+++ flake.nix
@@ -1,40 +1,77 @@
 {
   description = "honggfuzz-rs: Fuzz your Rust code with Google-developed Honggfuzz !";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-
-    flake-utils.url = "github:numtide/flake-utils";
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem ( system: let
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-    in rec {
-      packages.honggfuzz-rs = pkgs.rustPlatform.buildRustPackage rec {
-        pname = "honggfuzz-rs";
-        version = "git";
+  inputs = {
+    # nixpkgs = {
+    #   type = "indirect"; # take it from the registry
+    #   id   = "nixpkgs";
+    # };
 
-        src = ./.;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-        cargoLock = {
-          lockFile = ./Cargo.lock;
+    # this is the last version with glibc <= 2.37. Newer versions of glibc make honggfuzz fail to build, see https://github.com/google/honggfuzz/issues/518
+    nixpkgs-glibc237.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }; 
+  };
+
+  outputs = inputs @ {flake-parts, nixpkgs, nixpkgs-glibc237, ...}: flake-parts.lib.mkFlake { inherit inputs; } {
+    imports = [
+      inputs.devenv.flakeModule
+    ];
+    systems = nixpkgs.lib.systems.flakeExposed;
+
+    perSystem = {system, pkgs, self', ...}: let 
+      pkgs-glibc237 = nixpkgs-glibc237.legacyPackages.${system};
+    in {
+      packages = rec {
+        default = honggfuzz-rs;
+        honggfuzz-rs = pkgs-glibc237.rustPlatform.buildRustPackage rec {
+          pname = "honggfuzz-rs";
+          version = "git";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
         };
       };
 
-      devShells.default = pkgs.mkShell rec {
-        name = "honggfuzz-rs-devshell";
+      devenv.shells.default = {
+        stdenv = pkgs-glibc237.stdenv;
 
-        buildInputs = with pkgs; [
+        packages = with pkgs; [
           libbfd
           bintools-unwrapped
           libunwind
         ];
 
-        inputsFrom = with pkgs; [
-          packages.honggfuzz-rs
-        ];
+        languages = {
+          rust = {
+            enable = true;
+            channel = "stable";
+          };
+        };
       };
-
-      packages.default = packages.honggfuzz-rs;
-    });
+    };
+  };
 }
